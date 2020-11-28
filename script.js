5d627ee8c15a56ee7761c327f0440fb1f1e01ead
danielsilva <daniel-santos2000@live.com>
2020-11-28 00:17:12 -0300

    implementação

---
+++ script.js
@@ -0,0 +1,13 @@
+function avancar(id_pagina){
+    console.log('ESTA CHEGANDO NA FUNCAO AQUI EM ')
+
+
+
+    document.getElementById(id_pagina).style.display = 'block'
+
+
+}
+
+function voltar(){
+
+}
