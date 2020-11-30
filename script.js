3c8ecd4000a496f5600ba5482b80ebac799eaa37
danielsilva <daniel-santos2000@live.com>
2020-11-29 21:52:02 -0300

    primeira parte feita

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 function avancar(id_page_none,id_pag_avancar){
+
+
+    $(document).ready(function(){
+        $('.modal').modal();
+      })
+
     document.getElementById(id_page_none).style.display = 'none'
 
     document.getElementById(id_pag_avancar).style.display = 'block'
     document.getElementById(id_pag_avancar).style.display = 'flex'
-
 }
 
 function voltar(id_page_none,id_pag_voltar){
@@ -12,3 +17,14 @@ function voltar(id_page_none,id_pag_voltar){
     document.getElementById(id_pag_voltar).style.display = 'block'
     document.getElementById(id_pag_voltar).style.display = 'flex'
 }
+
+function abrirModal(opcao){
+
+    if(opcao === 'opcao-1'){    
+        console.log('aVAI ABRIR MODAL 1')
+    }
+}
+
+$(document).ready(function(){
+    $('.modal').modal();
+  });
